8b4d29a3a7fdbcca443d77f4cb1355ac26e26023
YiriyMakarov <local1984@example.com>
2024-05-27 20:19:55 +0300

    Update commands_handler.py

--- bot_dnd.py
+++ bot_dnd.py
@@ -6,6 +6,7 @@ from aiogram import Bot, Dispatcher
 from tok import bot_tok
 from main_menu import commands_handler
 from characters import command_characters, message_classes
+from tools import calc_handlers
 
 
 async def main():
@@ -15,10 +16,9 @@ async def main():
     bot = Bot(token=bot_tok)
 
     dp.include_router(commands_handler.router)
+    dp.include_router(calc_handlers.router)
     dp.include_router(message_classes.router)
     dp.include_router(command_characters.router)
-    # dp.include_router(add_product.router)
-    # dp.include_router(delete_product.router)
 
     await dp.start_polling(bot)
 
