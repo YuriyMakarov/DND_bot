5e2976c1fe3f5349a64cd359d383a25048436f61
YiriyMakarov <local1984@example.com>
2024-05-27 11:05:51 +0300

    Update commands.py

--- main_menu/commands.py
+++ main_menu/commands.py
@@ -2,12 +2,22 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-from main_menu.main_reply_kb import kb_main
+from data.collections_of_keyboards import main_keyboard
+from main_menu.def_keyboards import make_reply_keyboard
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer(text=f"Здравствуйте, {message.from_user.full_name}",
-                         reply_markup=kb_main())
+    await message.answer(
+        text=f"Dungeons&Dragons\n"
+             f"Здесь основная информация о Dungeons&Dragons\n"
+             f"Миры\n"
+             f"Лор доступных миров мастеров\n"
+             f"Инструменты\n"
+             f"Инструменты для расчетов различных механик\n"
+             f"Лист персонажей\n"
+             f"Набор сведений об ваших игровых персонажей",
+        reply_markup=make_reply_keyboard(main_keyboard)
+    )
