34b991f820227c68b1218ebc6a4b26a9085a3b12
Yuri_Makarov <111378790+YuriyMakarov@users.noreply.github.com>
2024-06-13 03:20:20 +0300

    Update bot_dnd.py

--- bot_dnd.py
+++ bot_dnd.py
@@ -3,7 +3,6 @@ import logging
 
 from aiogram import Bot, Dispatcher
 
-from tok import bot_tok
 from main_menu import commands_handler
 from characters import command_characters, message_classes
 from tools import calc_handlers
@@ -13,7 +12,7 @@ async def main():
     logging.basicConfig(level=logging.INFO)
 
     dp = Dispatcher()
-    bot = Bot(token=bot_tok)
+    bot = Bot(token="7255273514:AAFr0juF2PVIxtVB9_NDjBxladnoZpaBAvY")
 
     dp.include_router(commands_handler.router)
     dp.include_router(calc_handlers.router)
